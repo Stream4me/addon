0e12f276ee2ccf8019aee2b687f7a8af8f8cced0
aandroide <14141574+aandroide@users.noreply.github.com>
2025-02-24 23:05:05 +0100

    Update elementum_download.py
    fix errore elementum

--- platformcode/elementum_download.py
+++ platformcode/elementum_download.py
@@ -1,4 +1,4 @@
-
+import json
 from core import filetools, downloadtools, httptools, support
 from platformcode import config, platformtools, updater
 import xbmc, xbmcaddon, sys, platform
@@ -13,73 +13,7 @@ elementum_setting = filetools.join(setting_path,'plugin.video.elementum')
 elementum_setting_file = filetools.join(elementum_setting,'settings.xml')
 s4me_setting_file = filetools.join(addon_path,'plugin.video.s4me', 'resources', 'settings', 'elementum', 'settings.xml')
 
-def download(item=None):
-    ret = True
-
-    if filetools.exists(elementum_path):
-        # Check if Elementum is active
-        is_active = xbmc.getCondVisibility('System.HasAddon(plugin.video.elementum)')
-        if not is_active:
-            if platformtools.dialog_yesno(config.get_localized_string(70784), config.get_localized_string(70783)):
-                addon_file = filetools.file_open(filetools.join(elementum_path, 'addon.xml')).read()
-                required = support.match(addon_file, patron=r'addon="([^"]+)').matches
-                for r in required:
-                    xbmc.executebuiltin('InstallAddon(' + r + ')', wait=True)
-                setting()
-                platformtools.dialog_ok('Elementum', config.get_localized_string(70783))
-            else:
-                ret = False
-        else:
-            platformtools.dialog_ok(config.get_localized_string(70784), config.get_localized_string(90006))
-    else:
-        if platformtools.dialog_yesno(config.get_localized_string(70784), config.get_localized_string(70782)):
-            pform = get_platform()
-            real_elementum_url = elementum_url + '/expanded_assets/' + httptools.downloadpage(elementum_url + '/latest').url.split('/')[-1]
-            url = support.match(real_elementum_url, patron=r'<a href="([a-zA-Z0-9/\.-]+{}.zip)'.format(pform)).match
-            support.info('OS:', pform)
-            support.info('Extract IN:', elementum_path)
-            support.info('URL:', url)
-            if url:
-                dl = downloadtools.downloadfile(host + url, filename)
-                if dl == -3:
-                    filetools.remove(filename)
-                    dl = downloadtools.downloadfile(host + url, filename)
-                if dl is None:
-                    extract()
-                    xbmc.sleep(1000)
-                    addon_file = filetools.file_open(filetools.join(elementum_path, 'addon.xml')).read()
-                    required = support.match(addon_file, patron=r'addon="([^"]+)').matches
-                    for r in required:
-                        xbmc.executebuiltin('InstallAddon(' + r + ')', wait=True)
-                    setting()
-                else:
-                    ret = False
-            else:
-                ret = False
-        else:
-            ret = False
-    return ret
-
-def extract():
-    import zipfile
-    from platformcode.updater import fixZipGetHash
-    support.info('Estraggo Elementum in:', elementum_path)
-    try:
-        # hash = fixZipGetHash(filename)
-        # support.info(hash)
-
-        with zipfile.ZipFile(filetools.file_open(filename, 'rb', vfs=False)) as zip_ref:
-            zip_ref.extractall(xbmc.translatePath(addon_path))
-
-    except Exception as e:
-        support.info('Non sono riuscito ad estrarre il file zip')
-        support.logger.error(e)
-        import traceback
-        support.logger.error(traceback.print_exc())
-
-
 def setting():
-    # support.dbg()
     xbmc.executebuiltin('UpdateLocalAddons')
     xbmc.sleep(1000)
     if filetools.isfile(elementum_setting_file):
@@ -102,12 +36,71 @@ def setting():
         xbmc.sleep(1000)
         xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id":1, "method": "Addons.SetAddonEnabled", "params": { "addonid": "plugin.video.elementum", "enabled": true }}')
 
-
     updater.refreshLang()
 
     if filetools.exists(filename):
         filetools.remove(filename)
 
+def download(item=None):
+    ret = True
+
+    if filetools.exists(elementum_path):
+        response = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": {"addonid": "plugin.video.elementum", "properties": ["enabled"]}, "id": 1}')
+        result = json.loads(response)
+        if 'result' in result and 'addon' in result['result'] and result['result']['addon']['enabled']:
+            platformtools.dialog_ok(config.get_localized_string(70784), config.get_localized_string(90006))
+            return ret
+
+        if platformtools.dialog_yesno(config.get_localized_string(70784), config.get_localized_string(70783)):
+            addon_file = filetools.file_open(filetools.join(elementum_path,'addon.xml')).read()
+            required = support.match(addon_file, patron=r'addon="([^"]+)').matches
+            for r in required: xbmc.executebuiltin('InstallAddon(' + r + ')', wait=True)
+            setting()
+            platformtools.dialog_ok('Elementum', config.get_localized_string(70783))
+        else:
+            ret = False
+
+    else:
+        if platformtools.dialog_yesno(config.get_localized_string(70784), config.get_localized_string(70782)):
+            pform = get_platform()
+            real_elementum_url = elementum_url + '/expanded_assets/' + httptools.downloadpage(elementum_url +'/latest').url.split('/')[-1]
+            url = support.match(real_elementum_url, patron=r'<a href="([a-zA-Z0-9/\.-]+{}.zip)'.format(pform)).match
+            support.info('OS:', pform)
+            support.info('Extract IN:', elementum_path)
+            support.info('URL:', url)
+            if url:
+                dl = downloadtools.downloadfile(host + url, filename)
+                if dl == -3:
+                    filetools.remove(filename)
+                    dl = downloadtools.downloadfile(host + url, filename)
+                if dl == None:
+                    extract()
+                    xbmc.sleep(1000)
+                    addon_file = filetools.file_open(filetools.join(elementum_path,'addon.xml')).read()
+                    required = support.match(addon_file, patron=r'addon="([^"]+)').matches
+                    for r in required: xbmc.executebuiltin('InstallAddon(' + r + ')', wait=True)
+                    setting()
+                else:
+                    ret = False
+            else:
+                ret = False
+        else:
+            ret = False
+    return ret
+
+def extract():
+    import zipfile
+    from platformcode.updater import fixZipGetHash
+    support.info('Estraggo Elementum in:', elementum_path)
+    try:
+        with zipfile.ZipFile(filetools.file_open(filename, 'rb', vfs=False)) as zip_ref:
+            zip_ref.extractall(xbmc.translatePath(addon_path))
+
+    except Exception as e:
+        support.info('Non sono riuscito ad estrarre il file zip')
+        support.logger.error(e)
+        import traceback
+        support.logger.error(traceback.print_exc())
 
 def get_platform():
     build = xbmc.getInfoLabel("System.BuildVersion")
